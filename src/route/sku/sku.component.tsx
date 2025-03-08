774345beed84c4d64cba965b84e4c66b262922f6
Tushar Mehta <tm2582001@gmail.com>
2025-03-08 15:46:22 +0530

    added logic to delete sku and store

--- src/route/sku/sku.component.tsx
+++ src/route/sku/sku.component.tsx
@@ -1,23 +1,28 @@
-import { useAppSelector } from "../../utils/reducer/redux.util";
+import { useAppSelector, useAppDispatch } from "../../utils/reducer/redux.util";
+import { deleteSKU } from "../../store/sku/sku.action";
+import DeleteIcon from "../../assets/trash-solid.svg?react";
 
 
 import "../store/store.styles.css";
 
 const Sku = ()=>{
     const skus = useAppSelector((state) => state.skus.skus);
+  const dispatch = useAppDispatch();
     // console.log(skus);
-
+    const handleDelete = (id: number) => dispatch(deleteSKU(id));
     return  <div className="store-page-wrapper">
     <div className="store-page-container">
       <div className="grid-container grid-header">
+      <span className="grid-header-half-border"></span>
         <span className="grid-header-half-border">S.No</span>
         <span className="grid-header-full-border">SKU</span>
         <span className="grid-header-half-border">Price</span>
         <span className="grid-header-no-border">Cost</span>
       </div>
-      {skus.map((sku) => (
+      {skus.map((sku, index) => (
         <div key={sku.id} className="grid-container">
-          <span className="grid-header-half-border">{sku.index}</span>
+            <DeleteIcon className="delete-icon grid-header-no-border" onClick={() => handleDelete(sku.index)} />
+          <span className="grid-header-half-border">{index +1}</span>
           <span className="grid-container-full-border">{sku.label}</span>
           <span className="grid-header-half-border">{sku.price}</span>
           <span className="grid-header-no-border">{sku.cost}</span>
--- src/route/store/store.component.tsx
+++ src/route/store/store.component.tsx
@@ -1,24 +1,30 @@
-import { useAppSelector } from "../../utils/reducer/redux.util";
+import { useAppSelector, useAppDispatch } from "../../utils/reducer/redux.util";
+import { deleteStore } from "../../store/storehandler/store.action";
+import DeleteIcon from "../../assets/trash-solid.svg?react";
 
 import "./store.styles.css";
 
 const Store = () => {
   const stores = useAppSelector((state) => state.stores.stores);
+  const dispatch = useAppDispatch();
+  const handleDelete = (id: number) => dispatch(deleteStore(id));
 
-//   console.log(stores);
+  //   console.log(stores);
 
   return (
     <div className="store-page-wrapper">
       <div className="store-page-container">
         <div className="grid-container grid-header">
+          <span className="grid-header-half-border"></span>
           <span className="grid-header-half-border">S.No</span>
           <span className="grid-header-full-border">Store</span>
           <span className="grid-header-half-border">City</span>
           <span className="grid-header-no-border">State</span>
         </div>
-        {stores.map((store) => (
+        {stores.map((store, index) => (
           <div key={store.id} className="grid-container">
-            <span className="grid-header-half-border">{store.index}</span>
+            <DeleteIcon className="delete-icon grid-header-no-border" onClick={() => handleDelete(store.index)} />
+            <span className="grid-header-half-border">{index + 1}</span>
             <span className="grid-container-full-border">{store.label}</span>
             <span className="grid-header-half-border">{store.city}</span>
             <span className="grid-header-no-border">{store.state}</span>
